aa8b77d5294ee2f60f9370dc8909ce114bcb4a7b
xingyc2 <shikivin@gmail.com>
2024-01-30 17:00:44 -0500

    4

--- Magnetotransport measurement(UI).py
+++ Magnetotransport measurement(UI).py
@@ -9,6 +9,71 @@ import pandas as pd
 import device
 from GPIB_instruments import Agilent6613C_PowerSupply, Agilent2400_SourceMeter
 
-Agilent6613C_PowerSupply('GPIB0::5::INSTR')
-Agilent6613C_PowerSupply('GPIB0::9::INSTR')
-Agilent2400_SourceMeter('GPIB0::20::INSTR')
+class TransferCurve():
+    self.X_magnet_GPA = 132.3129
+    self.Y_magnet_GPA = 129.76684
+    self.max_curr_PS = 1
+    
+    def __init__(self):
+        #super().__init__()
+        self.initialize()
+
+    def initialize(self, x_address="GPIB0::5::INSTR", y_address="GPIB0::9::INSTR", sm_address="GPIB0::20::INSTR"):
+        PowerSupply_X = Agilent6613C_PowerSupply(x_address)
+        PowerSupply_Y = Agilent6613C_PowerSupply(y_address)
+        SourceMeter = Agilent2400_SourceMeter(sm_address)
+
+        PowerSupply_X.initialize()
+        PowerSupply_Y.initialize()
+        SourceMeter.initialize()
+    
+    def output_on(self, x_on=True, y_on=True, sm_on=True)
+        PowerSupply_X.output(x_on)
+        PowerSupply_Y.output(y_on)
+        SourceMeter.output(sm_on)
+        
+    def sweep_params_gauss(self, start_gauss=0, stop_gauss=self.X_magnet_GPA, step_gauss=0.0003*X_magnet_GPA, loop=1):
+        self.start_gauss = start_gauss
+        self.stop_gauss = stop_gauss
+        self.step_gauss = step_gauss
+        self.start_curr = start_gauss/X_magnet_GPA
+        self.stop_curr = stop_gauss/X_magnet_GPA
+        self.step_curr = step_gauss/X_magnet_GPA
+        slef.loop = loop
+        
+    def sweep_params_amps(self, start_curr=0, stop_curr=self.max_curr_PS, step_curr=0.0003, loop=1):
+        self.start_curr = start_curr
+        self.stop_curr = stop_curr
+        self.step_curr = step_curr
+        self.start_gauss = start_curr*X_magnet_GPA
+        self.stop_gauss = stop_curr*X_magnet_GPA
+        self.step_gauss = step_curr*X_magnet_GPA
+        slef.loop = loop
+    
+    def 
+
+# Sweep array parameter setup
+stop_gauss = X_magnet_GPA*1
+step_gauss = X_magnet_GPA*0.0003
+min_curr = start_gauss/X_magnet_GPA
+max_curr = stop_gauss/X_magnet_GPA
+step = step_gauss/X_magnet_GPA
+loop = 1
+# Square wave array
+curr_sample = 0.001
+pts_per_hfcycle = 1
+compliance = 50
+# Array of different magnitudes of biasing field
+bias_y_arr_gauss = [-10]
+bias_y_arr = np.array(bias_y_arr_gauss) / Y_magnet_GPA
+bias_ON = True
+live_display = 'DISABLED'                  # Options of 'DISABLED', 'WHOLE', 'TRACE', and 'WHOLE and TRACE'
+                                        # for observing the transfer curve measurement
+
+FourProbe.NPLC(0.03)
+
+
+
+X_field.output()
+Y_field.output()
+FourProbe.output()
