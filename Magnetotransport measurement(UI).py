6e9a96c8cc6f70fbedad72d479618bfeaf0ba9a7
xingyc2 <shikivin@gmail.com>
2024-01-31 17:49:16 -0500

    1

--- Magnetotransport measurement(UI).py
+++ Magnetotransport measurement(UI).py
@@ -17,8 +17,20 @@ class TransferCurve():
     def __init__(self):
         #super().__init__()
         self.initialize()
+    
+    def a_to_g(self, a, GPA='X'):
+        if GPA == 'X':
+            return a*self.X_magnet_GPA
+        elif GPA == 'Y':
+            return a*self.X_magnet_GPA
+    
+    def g_to_a(self, g, GPA='Y'):
+        if GPA == 'X':
+            return g/self.Y_magnet_GPA
+        elif GPA == 'Y':
+            return g/self.Y_magnet_GPA
 
-    def initialize(self, x_address="GPIB0::5::INSTR", y_address="GPIB0::9::INSTR", sm_address="GPIB0::20::INSTR"):
+    def initialize(self, x_address="GPIB0::5::INSTR", y_address="GPIB0::9::INSTR", sm_address="GPIB0::20::INSTR", NPLC=0.03):
         PowerSupply_X = Agilent6613C_PowerSupply(x_address)
         PowerSupply_Y = Agilent6613C_PowerSupply(y_address)
         SourceMeter = Agilent2400_SourceMeter(sm_address)
@@ -26,11 +38,12 @@ class TransferCurve():
         PowerSupply_X.initialize()
         PowerSupply_Y.initialize()
         SourceMeter.initialize()
+        SourceMeter.NPLC()
     
-    def output_on(self, x_on=True, y_on=True, sm_on=True)
-        PowerSupply_X.output(x_on)
-        PowerSupply_Y.output(y_on)
-        SourceMeter.output(sm_on)
+    def output_on(self, x_onoff=True, y_onoff=False, sm_onoff=True)
+        PowerSupply_X.output(x_onoff)
+        PowerSupply_Y.output(y_onoff)
+        SourceMeter.output(sm_onoff)
         
     def sweep_params_gauss(self, start_gauss=0, stop_gauss=self.X_magnet_GPA, step_gauss=0.0003*X_magnet_GPA, loop=1):
         self.start_gauss = start_gauss
@@ -49,8 +62,40 @@ class TransferCurve():
         self.stop_gauss = stop_curr*X_magnet_GPA
         self.step_gauss = step_curr*X_magnet_GPA
         slef.loop = loop
+
+    def curr_sample(self, curr_sample=0.001, pts_repeated=2, square_wave=False):
+        try:
+            if square_wave == True:
+                self.curr_sample_arr = np.repeat([curr_sample, -curr_sample],(pts_repeated))
+            elif square_wave == False:
+                self.curr_sample_arr = np.repeat(curr_sample, (pts_repeated))
+            else:
+                raise Exception()
+        except Exception as e:
+            raise Exception(f"TransferCurve: Invalid current sample input: {str(e)}")
+            
+    '''
+    def bias_y_arr(self, bias_ON=False):
+        try:
+            PowerSupply_Y.output(bias_ON)
+        except Exception as e:
+            raise Exception(f"TransferCurve: Invalid biasing Y field input: {str(e)}")
+        
+    def bias_y_arr(self, bias_ON=True, y_arr_gauss):
+        try:
+            if bias_ON == True:
+                PowerSupply_Y.output(bias_ON)
+                
+            else:
+                PowerSupply_Y.output(False)
+        except Exception as e:
+            raise Exception(f"TransferCurve: Invalid biasing Y field input: {str(e)}")
+        '''
     
     def 
+    
+    
+    
 
 # Sweep array parameter setup
 stop_gauss = X_magnet_GPA*1
